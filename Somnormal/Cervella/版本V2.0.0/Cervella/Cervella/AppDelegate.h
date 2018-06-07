518fa4e0afa1b78198100c14a37e158c40270169
Jiawei310 <1069588981@qq.com>
2018-06-07 17:31:13 +0800

    整理新建Cervella项目2.0

---
+++ Somnormal/Cervella/版本V2.0.0/Cervella/Cervella/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cervella
+//
+//  Created by 一磊 on 2018/6/6.
+//  Copyright © 2018年 NuoZhiJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Somnormal/Cervella/版本V2.0.0/Cervella/Cervella/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Cervella
+//
+//  Created by 一磊 on 2018/6/6.
+//  Copyright © 2018年 NuoZhiJia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
