815a8581741d74522f4d9a9cd0edef9d1c2d8630
Jiawei310 <1069588981@qq.com>
2018-10-16 22:25:04 +0800

    help

--- Somnormal/Cervella/版本V1.0/Cervella/Home/Menu/Help/HelpViewController.m
+++ Somnormal/Cervella/版本V1.0/Cervella/Home/Menu/Help/HelpViewController.m
@@ -43,7 +43,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-//    self.title = @"Help";
+    self.title = @"Help";
     
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName:[UIColor whiteColor]}];
